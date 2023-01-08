fc3b5a0740a8ec2e9c44f9bca2c005e15d2fa15c
João Caneo <joaovictornetocaneo@hotmail.com>
2023-01-08 11:14:43 -0300

    página contato e função context do render

--- recibos/views.py
+++ recibos/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 # Create your views here.
 def inicio(request):
-    return render(request, 'recibos/inicio.html')
+    return render(request, 'recibos/inicio.html', context={
+        'nome': 'João Victor',
+    })
+
 
 def contato(request):
-    return HttpResponse('/contato: página de CONTATO!')
+    return render(request=request, template_name='recibos/contato.html')
+
 
 def sobre(request):
-    return HttpResponse('/sobre: página sobre!')    
+    return HttpResponse('/sobre: página sobre!')
